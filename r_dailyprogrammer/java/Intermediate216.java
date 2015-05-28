52fbad1af90d6f24ca6b60fcbe2e6ed7bb2d5c75
Adrian Hintermaier <adrian.hintermaier@gmail.com>
2015-05-28 16:38:25 +0200

    [2015-05-28] Intermediate216 http://redd.it/37idka

--- r_dailyprogrammer/java/Intermediate216.java
+++ r_dailyprogrammer/java/Intermediate216.java
@@ -20,9 +20,6 @@ public class Intermediate216 {
 		theTurn(players, deck, communityCards);
 
 		theEnd(players, deck, communityCards);
-
-		// test();
-		// test2(players,deck);
 	}
 
 	private static void aiFoldDecision(ArrayList<Player> players, int threshold) {
@@ -33,44 +30,6 @@ public class Intermediate216 {
 				currentPlayer.fold();
 			}
 		}
-	}
-
-	private static void test2(ArrayList<Player> players, Deck deck) {
-		Card card1 = deck.pop();
-		Card card2 = deck.pop();
-		Card card3 = deck.pop();
-		Card card4 = deck.pop();
-		Card card5 = deck.pop();
-		players.get(0).updatePossibleHands(card1, card2, card3);
-		System.out.println(card1 + ", " + card2 + ", " + card3);
-		players.get(0).possibleHands.get(0).display();
-		players.get(0).updatePossibleHands(card4);
-		System.out.println(card4);
-		players.get(0).possibleHands.get(0).display();
-		players.get(0).updatePossibleHands(card5);
-		System.out.println(card5);
-		players.get(0).possibleHands.get(0).display();
-	}
-
-	private static void test() {
-		Deck deck = new Deck();
-		ArrayList<Card> cards = new ArrayList<Card>();
-		cards.add(new Card(0, 1));
-		cards.add(new Card(0, 2));
-		cards.add(new Card(0, 3));
-		cards.add(new Card(0, 4));
-		cards.add(new Card(0, 5));
-		Hand hand1 = new Hand(cards);
-		cards.clear();
-		cards.add(new Card(0, 3));
-		cards.add(new Card(0, 4));
-		cards.add(new Card(1, 5));
-		cards.add(new Card(0, 6));
-		cards.add(new Card(0, 7));
-		Hand hand2 = new Hand(cards);
-		hand1.display();
-		hand2.display();
-		System.out.println(hand1.compareTo(hand2));
 	}
 
 	public static void burnCard(Deck deck) {
